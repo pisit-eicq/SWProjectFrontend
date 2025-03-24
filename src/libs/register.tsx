9f8b18db8076b3368e767812ccf30e5f64cf46cd
PUN <tmr.parintorn@gmail.com>
2025-03-24 14:48:26 +0700

    fix reg

--- src/libs/register.tsx
+++ src/libs/register.tsx
@@ -1,16 +1,20 @@
 export default async function userRegister(name: string, email: string, password: string) {
-    const response = await fetch("https://a08-venue-explorer-backend.vercel.app/api/v1/auth/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name, email, password }),
-    });
-  
-    if (!response.ok) {
-      throw new Error("Failed to register user");
-    }
-  
-    return await response.json();
+  const response = await fetch("https://sw-project-backend-one.vercel.app/api/v1/auth/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      email,
+      password,
+      role: "user"  // เพิ่ม role ให้เป็น "user"
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to register user");
   }
-  
+
+  return await response.json();
+}
